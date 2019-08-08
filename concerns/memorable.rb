b3f3a59a1fa062f5dccf05ee0d0fd4cb3c7f62e9
jessi ortega <ortegabx05@gmail.com>
2019-08-08 18:03:13 +0000

    Automatically backed up by Learn

--- concerns/memorable.rb
+++ concerns/memorable.rb
@@ -1,8 +1,12 @@
 require_relative '../lib/concerns/memorable'
 
 module Memorable
-  
-  
-  
-  
-end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
