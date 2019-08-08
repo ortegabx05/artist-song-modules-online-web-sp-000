080780de0cbf9457b0b73556a165b1dd1987abf9
jessi ortega <ortegabx05@gmail.com>
2019-08-08 18:41:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,24 +8,25 @@ class Artist
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
   include Paramable::InstanceMethods
- 
+
   @@artists = []
+  
+  def self.all
+    @@artists
+  end
 
   def initialize
     super
     @songs = []
   end
 
-  def self.all
-    @@artists
-  end
-
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+
+end
