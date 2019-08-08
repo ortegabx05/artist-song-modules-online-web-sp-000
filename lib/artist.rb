3395a705419a7fde51f41f2d0caf17d05532e40c
jessi ortega <ortegabx05@gmail.com>
2019-08-08 18:04:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,12 +3,10 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-extend Memorable::ClassMethods
-  extend Findable::ClassMethods
+
+  extend Memorable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
-
+ 
   @@artists = []
 
   def initialize
